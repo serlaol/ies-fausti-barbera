36d2fac3c10f1dec00aeef1dc1666f6c79cf1eb7
serlaol <sergio.laguna.olmo@gmail.com>
2023-03-21 12:56:24 +0100

    Agregar ejercicio3.java

---
+++ programacion/Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Ejercicio3 {
+    public static void main(String args[]) {
+        //Muestra los goles encajados en total.
+      int GC=23;
+      int GL=46;
+      int GT= GC + GL;
+
+      System.out.println("Total de goles marcados "+ GT +" goles");
+    }
+}
